05cc481b9540246b2fb531832b212b9ce1fc9bfe
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-07-31 22:41:54 +0200

    Added Identity to the project

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using App.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration["ConnectionString"] ?? throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppIdentityDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +20,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+     using IServiceScope scope = app.Services
+        .GetRequiredService<IServiceScopeFactory>().CreateScope();
+    AppIdentityDbContext context = scope.ServiceProvider.GetService<AppIdentityDbContext>()
+        ?? throw new ApplicationException("AppIdentityDbContext was not found in the dependency injection container.");
+    context.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -23,5 +39,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
