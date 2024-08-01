b6ae395a4f875ad5c45b0bb9909a444257a0eb50
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-08-01 16:54:07 +0200

    Added Location class and its tests

---
+++ App/Areas/Location/Location.cs
@@ -0,0 +1,49 @@
+﻿using FluentResults;
+using Microsoft.IdentityModel.Tokens;
+
+namespace App.Areas.Location;
+
+public class Location : AggreggateRoot
+{
+    public Guid Id { get; }
+    public int BuildingNumber { get; }
+    public int Floor { get; }
+
+    public IEnumerable<Guid> Desks => _desks;
+    private List<Guid> _desks;
+
+    public Location(
+        Guid id,
+        int buildingNumber,
+        int floor,
+        List<Guid> desks
+    )
+    {
+        Id = id;
+        BuildingNumber = buildingNumber;
+        Floor = floor;
+        _desks = desks;
+        RaiseDomainEvent(new LocationCreatedEvent());
+    }
+
+    public Location(int buildingNumber, int floor)
+        : this(
+            Guid.NewGuid(),
+            buildingNumber,
+            floor,
+            []
+        )
+    {
+    }
+
+    public Result Remove()
+    {
+        if (_desks.IsNullOrEmpty() == false)
+        {
+            return Result.Fail("In order to be removed, this location musn't contain any desks.");
+        }
+
+        RaiseDomainEvent(new LocationRemovedEvent());
+        return Result.Ok();
+    }
+}
---
+++ Tests/Unit/LocationTests.cs
@@ -0,0 +1,46 @@
+﻿using App.Areas.Location;
+using FluentAssertions;
+using FluentResults;
+
+namespace Tests;
+
+public class LocationTests
+{
+    [Fact]
+    public void Constructor_ShouldRaiseDomainEvent()
+    {
+        Location location = new(1, 2);
+
+        location.DomainEvents.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void Remove_ShouldFail_WhenLocationHasDesks()
+    {
+        Location location = new(
+            Guid.NewGuid(),
+            1,
+            2,
+            [Guid.NewGuid()]);
+        
+        Result result = location.Remove();
+
+        result.IsFailed.Should().BeTrue();
+        location.DomainEvents.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void Remove_ShouldReturnSuccessAndRaiseDomainEvent_WhenLocationHasNoDesks()
+    {
+        Location location = new(
+            Guid.NewGuid(),
+            1,
+            2,
+            []);
+        
+        Result result = location.Remove();
+
+        result.IsSuccess.Should().BeTrue();
+        location.DomainEvents.Should().HaveCount(2);
+    }
+}
