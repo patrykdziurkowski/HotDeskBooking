d610541c702d9b8d4a17ab5d1076130deca64ec8
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-08-01 16:53:56 +0200

    Added classes to be used for Domain design

---
+++ App/Areas/Shared/Domain/AggreggateRoot.cs
@@ -0,0 +1,18 @@
+﻿namespace App;
+
+public abstract class AggreggateRoot
+{
+    private readonly List<DomainEvent> _domainEvents = [];
+    public IEnumerable<DomainEvent> DomainEvents => _domainEvents;
+
+    public void RaiseDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+}
