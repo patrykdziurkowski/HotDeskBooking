af79e158158c40d0cfecb38f67d29010e484007f
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-08-06 10:34:27 +0200

    Added a domain service for changing a desk

---
+++ App/Areas/DesksReservations/ReservationDeskChangeService.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+
+namespace App;
+
+public class ReservationDeskChangeService
+{
+    public Result ChangeDesk(Desk oldDesk, Desk newDesk, DateOnly currentDate)
+    {
+        if (oldDesk.Reservation is null)
+        {
+            return Result.Fail("The desk has no reservation to transfer.");
+        }
+
+        if (currentDate.AddDays(1) >= oldDesk.Reservation.StartDate)
+        {
+            return Result.Fail("Cannot change the reservation less than a day prior to it.");
+        }
+
+        Result result = newDesk.Reserve(
+            oldDesk.Reservation.StartDate,
+            oldDesk.Reservation.EndDate,
+            currentDate
+        );
+        if (result.IsFailed)
+        {
+            return result;
+        }
+        oldDesk.CancelReservation();
+        return Result.Ok();
+    }
+}
---
+++ Tests/Unit/ReservationDeskChangeServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using App;
+using App.Areas.Locations;
+using FluentAssertions;
+using FluentResults;
+
+namespace Tests;
+
+public class ReservationDeskChangeServiceTests
+{
+    [Fact]
+    public void DeskChange_ShouldFail_WhenFirstDeskHasNoReservationToTransfer()
+    {
+        ReservationDeskChangeService service = new();
+        DateOnly currentDate = new(2018, 6, 13);
+        Location location = new(5, 2);
+        Desk oldDesk = new(location.Id);
+        Desk newDesk = new(location.Id);
+
+        Result result = service.ChangeDesk(oldDesk, newDesk, currentDate);
+
+        result.IsFailed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DeskChange_ShouldFail_WhenHasReservationAndTryingToChangeItLessThan24HoursPrior()
+    {
+        ReservationDeskChangeService service = new();
+        DateOnly currentDate = new(2018, 6, 13);
+        DateOnly reservationDate = currentDate.AddDays(1);
+        Location location = new(5, 2);
+        Desk oldDesk = new(location.Id);
+        Desk newDesk = new(location.Id);
+        oldDesk.Reserve(
+            reservationDate,
+            currentDate
+        );
+
+        Result result = service.ChangeDesk(oldDesk, newDesk, currentDate);
+
+        result.IsFailed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DeskChange_ShouldFail_WhenReservingNewDeskFailed()
+    {
+        ReservationDeskChangeService service = new();
+        DateOnly currentDate = new(2018, 6, 13);
+        DateOnly reservationDate = currentDate.AddDays(5);
+        Location location = new(5, 2);
+        Desk oldDesk = new(location.Id);
+        Desk newDesk = new(location.Id);
+        oldDesk.Reserve(
+            reservationDate,
+            currentDate
+        );
+        newDesk.Reserve(
+            reservationDate,
+            currentDate
+        );
+
+        Result result = service.ChangeDesk(oldDesk, newDesk, currentDate);
+
+        result.IsFailed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DeskChange_ShouldRaiseDomainEvents_WhenSuccessful()
+    {
+        ReservationDeskChangeService service = new();
+        DateOnly currentDate = new(2018, 6, 13);
+        DateOnly reservationDate = currentDate.AddDays(5);
+        Location location = new(5, 2);
+        Desk oldDesk = new(location.Id);
+        Desk newDesk = new(location.Id);
+        oldDesk.Reserve(
+            reservationDate,
+            currentDate
+        );
+
+        Result result = service.ChangeDesk(oldDesk, newDesk, currentDate);
+
+        result.IsSuccess.Should().BeTrue();
+        oldDesk.DomainEvents.Should().HaveCount(3);
+        newDesk.DomainEvents.Should().HaveCount(2);
+    }
+}
