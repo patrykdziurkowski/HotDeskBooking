deaffcaf2fec3c7a6802ed8fcc00dffef652ed93
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-08-06 11:27:53 +0200

    Added the missing SaveAsync overload to the interface

--- App/Areas/DesksReservations/IDeskRepository.cs
+++ App/Areas/DesksReservations/IDeskRepository.cs
@@ -8,4 +8,5 @@ public interface IDeskRepository
     public Task<List<Desk>> GetByLocationAsync(Guid locationId);
     public Task<Desk?> GetByIdAsync(Guid id);
     public Task SaveAsync(Desk desk);
+    public Task SaveAsync(List<Desk> desks);
 }
